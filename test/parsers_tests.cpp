7b80391613f30607207868b3d22890b7b5ec3645
tim95bell <tim95bell@gmail.com>
2025-03-28 18:07:50 +1100

    add tests for parsers::newline, and parsers::line

--- test/parsers_tests.cpp
+++ test/parsers_tests.cpp
@@ -10,28 +10,127 @@ using namespace std::literals::string_view_literals;
 
 TEST_CASE("character", "[parsers]") {
     SECTION("empty input") {
-        auto result = character(""sv);
-        CHECK(!result.has_value());
+        const auto result = character(""sv);
+        REQUIRE(!result.has_value());
     }
 
     SECTION("one character input") {
-        auto result = character("a"sv);
-        CHECK(result.has_value());
+        const auto result = character("a"sv);
+        REQUIRE(result.has_value());
         CHECK(result.value().first == 'a');
         CHECK(result.value().second.empty());
     }
 
     SECTION("two character input") {
-        auto result = character("ab"sv);
-        CHECK(result.has_value());
+        const auto result = character("ab"sv);
+        REQUIRE(result.has_value());
         CHECK(result.value().first == 'a');
         CHECK(result.value().second == "b"sv);
     }
 
     SECTION("many character input") {
-        auto result = character("hello"sv);
-        CHECK(result.has_value());
+        const auto result = character("hello"sv);
+        REQUIRE(result.has_value());
         CHECK(result.value().first == 'h');
         CHECK(result.value().second == "ello"sv);
+    }
+}
+
+TEST_CASE("newline", "[parsers]") {
+    SECTION("empty input") {
+        const auto result = newline(""sv);
+        REQUIRE(!result.has_value());
+    }
+
+    SECTION("one character input, no match") {
+        const auto result = newline("a"sv);
+        REQUIRE(!result.has_value());
+    }
+
+    SECTION("one character input, match") {
+        const auto result = newline("\n"sv);
+        REQUIRE(result.has_value());
+        CHECK(result.value().first == '\n');
+        CHECK(result.value().second.empty());
+    }
+
+    SECTION("two character input, no match") {
+        const auto result = newline("a\n"sv);
+        REQUIRE(!result.has_value());
+    }
+
+    SECTION("two character input, match") {
+        const auto result = newline("\na"sv);
+        REQUIRE(result.has_value());
+        CHECK(result.value().first == '\n');
+        CHECK(result.value().second == "a"sv);
+    }
+
+    SECTION("two character input, both newline, match") {
+        const auto result = newline("\n\n"sv);
+        REQUIRE(result.has_value());
+        CHECK(result.value().first == '\n');
+        CHECK(result.value().second == "\n"sv);
+    }
+
+    SECTION("many character input, no match") {
+        const auto result = newline("hel\nlo\n"sv);
+        REQUIRE(!result.has_value());
+    }
+
+    SECTION("many character input, match") {
+        const auto result = newline("\nhel\nlo\n"sv);
+        REQUIRE(result.has_value());
+        CHECK(result.value().first == '\n');
+        CHECK(result.value().second == "hel\nlo\n"sv);
+    }
+}
+
+TEST_CASE("line", "[parsers]") {
+    SECTION("empty input") {
+        const auto result = line(""sv);
+        REQUIRE(!result.has_value());
+    }
+
+    SECTION("one character input") {
+        const auto result = line("a"sv);
+        REQUIRE(result.has_value());
+        CHECK(result.value().first == "a"sv);
+        CHECK(result.value().second.empty());
+    }
+
+    SECTION("one character input, newline character") {
+        const auto result = line("\n"sv);
+        REQUIRE(result.has_value());
+        CHECK(result.value().first == ""sv);
+        CHECK(result.value().second.empty());
+    }
+
+    SECTION("many character input") {
+        const auto result = line("hello"sv);
+        REQUIRE(result.has_value());
+        CHECK(result.value().first == "hello"sv);
+        CHECK(result.value().second.empty());
+    }
+
+    SECTION("many character input, ending in newline") {
+        const auto result = line("hello\n"sv);
+        REQUIRE(result.has_value());
+        CHECK(result.value().first == "hello"sv);
+        CHECK(result.value().second.empty());
+    }
+
+    SECTION("many character input, newline in middle") {
+        const auto result = line("hello\nworld"sv);
+        REQUIRE(result.has_value());
+        CHECK(result.value().first == "hello"sv);
+        CHECK(result.value().second == "world"sv);
+    }
+
+    SECTION("many character input, starting with newline") {
+        const auto result = line("\nhello"sv);
+        REQUIRE(result.has_value());
+        CHECK(result.value().first == ""sv);
+        CHECK(result.value().second == "hello"sv);
     }
 }
