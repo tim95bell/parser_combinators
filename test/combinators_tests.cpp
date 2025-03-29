3931b157ccdd9f13374f74bf9dd54de2d6dce817
tim95bell <tim95bell@gmail.com>
2025-03-29 13:03:50 +1100

    don't fully use library namespace in tests

--- test/combinators_tests.cpp
+++ test/combinators_tests.cpp
@@ -5,95 +5,96 @@
 #include <catch2/catch_test_macros.hpp>
 #include <string_view>
 
-using namespace pc::combinators;
-using namespace pc::parsers;
-using namespace pc;
+namespace pc {
+    using namespace combinators;
+    using namespace parsers;
+}
 using namespace std::literals::string_view_literals;
 
 TEST_CASE("manyn", "[combinators]") {
     SECTION("manyn<0> with empty input, no match") {
-        const auto result = manyn<0>(tag("hello"))("");
+        const auto result = pc::manyn<0>(pc::tag("hello"))("");
         REQUIRE(result.has_value());
         CHECK(result.value().first.empty());
         CHECK(result.value().second.empty());
     }
 
     SECTION("manyn<0> with empty input, match") {
-        const auto result = manyn<0>(tag(""))("");
+        const auto result = pc::manyn<0>(pc::tag(""))("");
         REQUIRE(result.has_value());
         CHECK(result.value().first.empty());
         CHECK(result.value().second.empty());
     }
 
     SECTION("manyn<0> with non empty input, no match") {
-        const auto result = manyn<0>(tag("hello"))("worldworld");
+        const auto result = pc::manyn<0>(pc::tag("hello"))("worldworld");
         REQUIRE(result.has_value());
         CHECK(result.value().first.empty());
         CHECK(result.value().second == "worldworld"sv);
     }
 
     SECTION("manyn<0> with non empty input, match") {
-        const auto result = manyn<0>(tag("hello"))("hellohello");
+        const auto result = pc::manyn<0>(pc::tag("hello"))("hellohello");
         REQUIRE(result.has_value());
         CHECK(result.value().first.empty());
         CHECK(result.value().second == "hellohello"sv);
     }
 
     SECTION("manyn<1> with empty input, no match") {
-        const auto result = manyn<1>(tag("hello"))("");
+        const auto result = pc::manyn<1>(pc::tag("hello"))("");
         REQUIRE(!result.has_value());
     }
 
     SECTION("manyn<1> with empty input, match") {
-        const auto result = manyn<1>(tag(""))("");
+        const auto result = pc::manyn<1>(pc::tag(""))("");
         REQUIRE(result.has_value());
         CHECK(result.value().first == std::array<std::string, 1>{""});
         CHECK(result.value().second.empty());
     }
 
     SECTION("manyn<1> with non empty input, no match") {
-        const auto result = manyn<1>(tag("hello"))("worldworld");
+        const auto result = pc::manyn<1>(pc::tag("hello"))("worldworld");
         REQUIRE(!result.has_value());
     }
 
     SECTION("manyn<1> with non empty input, match") {
-        const auto result = manyn<1>(tag("hello"))("hellohello");
+        const auto result = pc::manyn<1>(pc::tag("hello"))("hellohello");
         REQUIRE(result.has_value());
         CHECK(result.value().first == std::array<std::string, 1>{"hello"});
         CHECK(result.value().second == "hello"sv);
     }
 
     SECTION("manyn<3> with empty input, no match") {
-        const auto result = manyn<3>(tag("hello"))("");
+        const auto result = pc::manyn<3>(pc::tag("hello"))("");
         REQUIRE(!result.has_value());
     }
 
     SECTION("manyn<3> with empty input, match") {
-        const auto result = manyn<3>(tag(""))("");
+        const auto result = pc::manyn<3>(pc::tag(""))("");
         REQUIRE(result.has_value());
         CHECK(result.value().first == std::array<std::string, 3>{"", "", ""});
         CHECK(result.value().second.empty());
     }
 
     SECTION("manyn<3> with empty input, match by unit") {
-        const auto result = manyn<3>(unit<int>(20))("");
+        const auto result = pc::manyn<3>(pc::unit<int>(20))("");
         REQUIRE(result.has_value());
         CHECK(result.value().first == std::array<int, 3>{20, 20, 20});
         CHECK(result.value().second.empty());
     }
 
     SECTION("manyn<3> with non empty input, no match") {
-        const auto result = manyn<3>(tag("hello"))("worldworld");
+        const auto result = pc::manyn<3>(pc::tag("hello"))("worldworld");
         REQUIRE(!result.has_value());
     }
 
     SECTION("manyn<3> with non empty input, partial match") {
-        const auto result = manyn<3>(tag("hello"))("hellohelloworld");
+        const auto result = pc::manyn<3>(pc::tag("hello"))("hellohelloworld");
         REQUIRE(!result.has_value());
     }
 
     SECTION("manyn<3> with non empty input, match") {
-        const auto result = manyn<3>(tag("hello"))("hellohellohellohello");
+        const auto result = pc::manyn<3>(pc::tag("hello"))("hellohellohellohello");
         REQUIRE(result.has_value());
         CHECK(result.value().first == std::array<std::string, 3>{"hello", "hello", "hello"});
         CHECK(result.value().second == "hello"sv);
@@ -102,36 +103,36 @@ TEST_CASE("manyn", "[combinators]") {
 
 TEST_CASE("trim", "[combinators]") {
     SECTION("trim with input that does NOT need trimming, match") {
-        const auto result = trim(tag("hello"))("hello");
+        const auto result = pc::trim(pc::tag("hello"))("hello");
         REQUIRE(result.has_value());
         CHECK(result.value().first == "hello"sv);
         CHECK(result.value().second.empty());
     }
 
     SECTION("trim with input that does NOT need trimming, no match") {
-        const auto result = trim(tag("hello"))("world");
+        const auto result = pc::trim(pc::tag("hello"))("world");
         REQUIRE(!result.has_value());
     }
 
     SECTION("trim with input that needs trimming, match") {
-        const auto result = trim(tag("hello"))(" \n \t  hello \n\t ");
+        const auto result = pc::trim(pc::tag("hello"))(" \n \t  hello \n\t ");
         REQUIRE(result.has_value());
         CHECK(result.value().first == "hello"sv);
         CHECK(result.value().second.empty());
     }
 
     SECTION("trim with input that needs trimming, no match") {
-        const auto result = trim(tag("hello"))("   \n\t world\n\t \t ");
+        const auto result = pc::trim(pc::tag("hello"))("   \n\t world\n\t \t ");
         REQUIRE(!result.has_value());
     }
 
     SECTION("trim with input that needs NOT trimming, would be match") {
-        const auto result = trim(tag(" \n \t  "))(" \n \t  hello \n\t ");
+        const auto result = pc::trim(pc::tag(" \n \t  "))(" \n \t  hello \n\t ");
         REQUIRE(!result.has_value());
     }
 
     SECTION("NO trim with input that needs NOT trimming, match") {
-        const auto result = tag(" \n \t  ")(" \n \t  hello \n\t ");
+        const auto result = pc::tag(" \n \t  ")(" \n \t  hello \n\t ");
         REQUIRE(result.has_value());
         CHECK(result.value().first == " \n \t  "sv);
         CHECK(result.value().second == "hello \n\t "sv);
@@ -139,38 +140,38 @@ TEST_CASE("trim", "[combinators]") {
 }
 
 TEST_CASE("choice", "[combinators]") {
-    const auto hello = tag("hello");
-    const auto world = tag("world");
-    const auto hel = tag("hel");
+    const auto hello = pc::tag("hello");
+    const auto world = pc::tag("world");
+    const auto hel = pc::tag("hel");
 
     SECTION("choice with no match") {
-        const auto result = choice(hello, world)("parser combinators");
+        const auto result = pc::choice(hello, world)("parser combinators");
         REQUIRE(!result.has_value());
     }
 
     SECTION("choice with first match") {
-        const auto result = choice(hello, world)("helloworld");
+        const auto result = pc::choice(hello, world)("helloworld");
         REQUIRE(result.has_value());
         CHECK(result.value().first == "hello"sv);
         CHECK(result.value().second == "world"sv);
     }
 
     SECTION("choice with second match") {
-        const auto result = choice(hello, world)("worldhello");
+        const auto result = pc::choice(hello, world)("worldhello");
         REQUIRE(result.has_value());
         CHECK(result.value().first == "world"sv);
         CHECK(result.value().second == "hello"sv);
     }
 
     SECTION("choice with both match, should be first") {
-        const auto result = choice(hello, hel)("helloworld");
+        const auto result = pc::choice(hello, hel)("helloworld");
         REQUIRE(result.has_value());
         CHECK(result.value().first == "hello"sv);
         CHECK(result.value().second == "world"sv);
     }
 
     SECTION("choice with both match (other way around), should be first") {
-        const auto result = choice(hel, hello)("helloworld");
+        const auto result = pc::choice(hel, hello)("helloworld");
         REQUIRE(result.has_value());
         CHECK(result.value().first == "hel"sv);
         CHECK(result.value().second == "loworld"sv);
@@ -178,29 +179,32 @@ TEST_CASE("choice", "[combinators]") {
 }
 
 TEST_CASE("many0_to_many1", "[combinators]") {
-    const auto empty = unit(std::vector<int>{});
-    const auto one = unit(std::vector<int>{1});
-    const auto many = unit(std::vector<int>{1, 2, 3});
+    const auto empty = pc::unit(std::vector<int>{});
+    const auto one = pc::unit(std::vector<int>{1});
+    const auto many = pc::unit(std::vector<int>{1, 2, 3});
 
     SECTION("empty output fails") {
-        const auto result = many0_to_many1(empty)("hello world");
+        const auto result = pc::many0_to_many1(empty)("hello world");
         REQUIRE(!result.has_value());
     }
 
     SECTION("failure fails") {
-        const auto result = many0_to_many1(many0(tag("world")))("hello world");
+        const auto result = pc::many0_to_many1(pc::many0(pc::tag("world")))("hello world");
+        // TODO(TB): why does this work without pc namespace? but the next line does not
+        // const auto result = many0_to_many1(pc::many0(pc::tag("world")))("hello world");
+        // const auto result2 = many0_to_many1(pc::tag("world"))("hello world");
         REQUIRE(!result.has_value());
     }
 
     SECTION("size one output succeeds") {
-        const auto result = many0_to_many1(one)("hello world");
+        const auto result = pc::many0_to_many1(one)("hello world");
         REQUIRE(result.has_value());
         CHECK(result.value().first == std::vector<int>{1});
         CHECK(result.value().second == "hello world"sv);
     }
 
     SECTION("size many output succeeds") {
-        const auto result = many0_to_many1(many)("hello world");
+        const auto result = pc::many0_to_many1(many)("hello world");
         REQUIRE(result.has_value());
         CHECK(result.value().first == std::vector<int>{1, 2, 3});
         CHECK(result.value().second == "hello world"sv);
--- test/parsers_tests.cpp
+++ test/parsers_tests.cpp
@@ -4,32 +4,33 @@
 #include <catch2/catch_test_macros.hpp>
 #include <string_view>
 
-using namespace pc::parsers;
-using namespace pc;
+namespace pc {
+    using namespace parsers;
+}
 using namespace std::literals::string_view_literals;
 
 TEST_CASE("character", "[parsers]") {
     SECTION("empty input") {
-        const auto result = character(""sv);
+        const auto result = pc::character(""sv);
         REQUIRE(!result.has_value());
     }
 
     SECTION("one character input") {
-        const auto result = character("a"sv);
+        const auto result = pc::character("a"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == 'a');
         CHECK(result.value().second.empty());
     }
 
     SECTION("two character input") {
-        const auto result = character("ab"sv);
+        const auto result = pc::character("ab"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == 'a');
         CHECK(result.value().second == "b"sv);
     }
 
     SECTION("many character input") {
-        const auto result = character("hello"sv);
+        const auto result = pc::character("hello"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == 'h');
         CHECK(result.value().second == "ello"sv);
@@ -38,48 +39,48 @@ TEST_CASE("character", "[parsers]") {
 
 TEST_CASE("newline", "[parsers]") {
     SECTION("empty input") {
-        const auto result = newline(""sv);
+        const auto result = pc::newline(""sv);
         REQUIRE(!result.has_value());
     }
 
     SECTION("one character input, no match") {
-        const auto result = newline("a"sv);
+        const auto result = pc::newline("a"sv);
         REQUIRE(!result.has_value());
     }
 
     SECTION("one character input, match") {
-        const auto result = newline("\n"sv);
+        const auto result = pc::newline("\n"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == '\n');
         CHECK(result.value().second.empty());
     }
 
     SECTION("two character input, no match") {
-        const auto result = newline("a\n"sv);
+        const auto result = pc::newline("a\n"sv);
         REQUIRE(!result.has_value());
     }
 
     SECTION("two character input, match") {
-        const auto result = newline("\na"sv);
+        const auto result = pc::newline("\na"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == '\n');
         CHECK(result.value().second == "a"sv);
     }
 
     SECTION("two character input, both newline, match") {
-        const auto result = newline("\n\n"sv);
+        const auto result = pc::newline("\n\n"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == '\n');
         CHECK(result.value().second == "\n"sv);
     }
 
     SECTION("many character input, no match") {
-        const auto result = newline("hel\nlo\n"sv);
+        const auto result = pc::newline("hel\nlo\n"sv);
         REQUIRE(!result.has_value());
     }
 
     SECTION("many character input, match") {
-        const auto result = newline("\nhel\nlo\n"sv);
+        const auto result = pc::newline("\nhel\nlo\n"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == '\n');
         CHECK(result.value().second == "hel\nlo\n"sv);
@@ -88,47 +89,47 @@ TEST_CASE("newline", "[parsers]") {
 
 TEST_CASE("line", "[parsers]") {
     SECTION("empty input") {
-        const auto result = line(""sv);
+        const auto result = pc::line(""sv);
         REQUIRE(!result.has_value());
     }
 
     SECTION("one character input") {
-        const auto result = line("a"sv);
+        const auto result = pc::line("a"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == "a"sv);
         CHECK(result.value().second.empty());
     }
 
     SECTION("one character input, newline character") {
-        const auto result = line("\n"sv);
+        const auto result = pc::line("\n"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == ""sv);
         CHECK(result.value().second.empty());
     }
 
     SECTION("many character input") {
-        const auto result = line("hello"sv);
+        const auto result = pc::line("hello"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == "hello"sv);
         CHECK(result.value().second.empty());
     }
 
     SECTION("many character input, ending in newline") {
-        const auto result = line("hello\n"sv);
+        const auto result = pc::line("hello\n"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == "hello"sv);
         CHECK(result.value().second.empty());
     }
 
     SECTION("many character input, newline in middle") {
-        const auto result = line("hello\nworld"sv);
+        const auto result = pc::line("hello\nworld"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == "hello"sv);
         CHECK(result.value().second == "world"sv);
     }
 
     SECTION("many character input, starting with newline") {
-        const auto result = line("\nhello"sv);
+        const auto result = pc::line("\nhello"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == ""sv);
         CHECK(result.value().second == "hello"sv);
@@ -137,55 +138,55 @@ TEST_CASE("line", "[parsers]") {
 
 TEST_CASE("tag", "[parsers]") {
     SECTION("empty input") {
-        const auto result = tag("hello")("");
+        const auto result = pc::tag("hello")("");
         REQUIRE(!result.has_value());
     }
 
     SECTION("empty input, empty tag") {
-        const auto result = tag("")("");
+        const auto result = pc::tag("")("");
         REQUIRE(result.has_value());
         CHECK(result.value().first.empty());
         CHECK(result.value().second.empty());
     }
 
     SECTION("non matching input") {
-        const auto result = tag("hello")("helo");
+        const auto result = pc::tag("hello")("helo");
         REQUIRE(!result.has_value());
     }
 
     SECTION("non matching input, matching in middle") {
-        const auto result = tag("hello")("helohellohelo");
+        const auto result = pc::tag("hello")("helohellohelo");
         REQUIRE(!result.has_value());
     }
 
     SECTION("non matching input, matching at end") {
-        const auto result = tag("hello")("helohello");
+        const auto result = pc::tag("hello")("helohello");
         REQUIRE(!result.has_value());
     }
 
     SECTION("non empty input, empty tag") {
-        const auto result = tag("")("hello");
+        const auto result = pc::tag("")("hello");
         REQUIRE(result.has_value());
         CHECK(result.value().first.empty());
         CHECK(result.value().second == "hello"sv);
     }
 
     SECTION("exactly matching input") {
-        const auto result = tag("hello")("hello");
+        const auto result = pc::tag("hello")("hello");
         REQUIRE(result.has_value());
         CHECK(result.value().first == "hello"sv);
         CHECK(result.value().second.empty());
     }
 
     SECTION("matching input, extra after") {
-        const auto result = tag("hello")("hello123");
+        const auto result = pc::tag("hello")("hello123");
         REQUIRE(result.has_value());
         CHECK(result.value().first == "hello"sv);
         CHECK(result.value().second == "123"sv);
     }
 
     SECTION("matching twice input") {
-        const auto result = tag("hello")("hellohello");
+        const auto result = pc::tag("hello")("hellohello");
         REQUIRE(result.has_value());
         CHECK(result.value().first == "hello"sv);
         CHECK(result.value().second == "hello"sv);
@@ -194,28 +195,28 @@ TEST_CASE("tag", "[parsers]") {
 
 TEST_CASE("unit", "[parsers]") {
     SECTION("unit<int> with empty input") {
-        const auto result = unit(20)(""sv);
+        const auto result = pc::unit(20)(""sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == 20);
         CHECK(result.value().second.empty());
     }
 
     SECTION("unit<std::string> with empty input") {
-        const auto result = unit(std::string("hello"))(""sv);
+        const auto result = pc::unit(std::string("hello"))(""sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == std::string("hello"));
         CHECK(result.value().second.empty());
     }
 
     SECTION("unit<int> with non empty input") {
-        const auto result = unit(20)("hello"sv);
+        const auto result = pc::unit(20)("hello"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == 20);
         CHECK(result.value().second == "hello"sv);
     }
 
     SECTION("unit<std::string> with non empty input") {
-        const auto result = unit(std::string("hello"))("world"sv);
+        const auto result = pc::unit(std::string("hello"))("world"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == std::string("hello"));
         CHECK(result.value().second == "world"sv);
@@ -226,45 +227,45 @@ TEST_CASE("first_char_match", "[parsers]") {
     const auto is_a = [](char x) { return x == 'a'; };
 
     SECTION("empty input, no match") {
-        const auto result = first_char_match(is_a)(""sv);
+        const auto result = pc::first_char_match(is_a)(""sv);
         REQUIRE(!result.has_value());
     }
 
     SECTION("non empty input, no match") {
-        const auto result = first_char_match(is_a)("hello"sv);
+        const auto result = pc::first_char_match(is_a)("hello"sv);
         REQUIRE(!result.has_value());
     }
 
     SECTION("non empty input, match first character") {
-        const auto result = first_char_match(is_a)("ahello"sv);
+        const auto result = pc::first_char_match(is_a)("ahello"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == 'a');
         CHECK(result.value().second == "hello"sv);
     }
 
     SECTION("non empty input, match middle character") {
-        const auto result = first_char_match(is_a)("helloaworld"sv);
+        const auto result = pc::first_char_match(is_a)("helloaworld"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == 'a');
         CHECK(result.value().second == "world"sv);
     }
 
     SECTION("non empty input, match last character") {
-        const auto result = first_char_match(is_a)("helloa"sv);
+        const auto result = pc::first_char_match(is_a)("helloa"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == 'a');
         CHECK(result.value().second.empty());
     }
 
     SECTION("non empty input, match middle character with multiple contiguous matches") {
-        const auto result = first_char_match(is_a)("helloaaworld"sv);
+        const auto result = pc::first_char_match(is_a)("helloaaworld"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == 'a');
         CHECK(result.value().second == "aworld"sv);
     }
 
     SECTION("non empty input, match middle character with multiple non contiguous matches") {
-        const auto result = first_char_match(is_a)("helloaworldaparser"sv);
+        const auto result = pc::first_char_match(is_a)("helloaworldaparser"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == 'a');
         CHECK(result.value().second == "worldaparser"sv);
@@ -275,45 +276,45 @@ TEST_CASE("last_char_match", "[parsers]") {
     const auto is_a = [](char x) { return x == 'a'; };
 
     SECTION("empty input, no match") {
-        const auto result = last_char_match(is_a)(""sv);
+        const auto result = pc::last_char_match(is_a)(""sv);
         REQUIRE(!result.has_value());
     }
 
     SECTION("non empty input, no match") {
-        const auto result = last_char_match(is_a)("hello"sv);
+        const auto result = pc::last_char_match(is_a)("hello"sv);
         REQUIRE(!result.has_value());
     }
 
     SECTION("non empty input, match first character") {
-        const auto result = last_char_match(is_a)("ahello"sv);
+        const auto result = pc::last_char_match(is_a)("ahello"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == 'a');
         CHECK(result.value().second.empty());
     }
 
     SECTION("non empty input, match middle character") {
-        const auto result = last_char_match(is_a)("helloaworld"sv);
+        const auto result = pc::last_char_match(is_a)("helloaworld"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == 'a');
         CHECK(result.value().second == "hello"sv);
     }
 
     SECTION("non empty input, match last character") {
-        const auto result = last_char_match(is_a)("helloa"sv);
+        const auto result = pc::last_char_match(is_a)("helloa"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == 'a');
         CHECK(result.value().second == "hello"sv);
     }
 
     SECTION("non empty input, match middle character with multiple contiguous matches") {
-        const auto result = last_char_match(is_a)("helloaaworld"sv);
+        const auto result = pc::last_char_match(is_a)("helloaaworld"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == 'a');
         CHECK(result.value().second == "helloa"sv);
     }
 
     SECTION("non empty input, match middle character with multiple non contiguous matches") {
-        const auto result = last_char_match(is_a)("helloaworldaparser"sv);
+        const auto result = pc::last_char_match(is_a)("helloaworldaparser"sv);
         REQUIRE(result.has_value());
         CHECK(result.value().first == 'a');
         CHECK(result.value().second == "helloaworldap"sv);
